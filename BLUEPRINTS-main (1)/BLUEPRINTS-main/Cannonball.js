2744970ab19ca5e7ce242106fd2a28e3b6b2c072
MonserratBedolla <99926583+MonserratBedolla@users.noreply.github.com>
2023-01-19 20:17:04 -0600

    Add files via upload

---
+++ BLUEPRINTS-main (1)/BLUEPRINTS-main/Cannonball.js
@@ -0,0 +1,34 @@
+class CannonBall{
+    constructor(x,y,){
+        var options={
+            restitution:0.8,
+            friction:0.1,
+            density:1.0,
+            isStatic:true
+        };
+        this.r=40;
+        this.body= Bodies.circle(x,y,this.r,options);
+        this.image = loadImage("./assets/cannonball.png")
+        World.add(world, this.body);
+    }
+    display(){
+        var angle = this.body.angle;
+        var pos = this.body.position;
+        push();
+        translate (pos.x, pos.y);
+        rotate (angle);
+        imageMode(CENTER);
+        //this.r al final
+        image (this.image,0,0,this.r,this.r);
+        pop();
+    }
+    shoot(){
+        //fromAngle con una l
+        var velocity=p5.Vector.fromAngle(cannon.angle);
+        velocity.mult(20);
+        Matter.Body.setStatic(this.body,false);
+        //setVelocity con l
+        Matter.Body.setVelocity(this.body,{x:velocity.x,y:velocity.y});
+
+    }
+}
---
+++ BLUEPRINTS-main (1)/BLUEPRINTS-main/sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+var tower;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+  cannonBallImage = loadImage("./assets/cannonball.png");
+}
+
+
+function setup() {
+  canvas = createCanvas(1200,600);
+
+  engine = Engine.create();
+  world = engine.world;
+  angle=-PI/4;
+
+  tower = new Tower(150,350,160,310);
+  cannon = new Cannon(180,110,100,50,angle);
+  cannonBall = new CannonBall(cannon.x,cannon.y);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+function draw() 
+{
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  Engine.update(engine);
+  tower.display();
+  cannon.display();
+  cannonBall.display();
+}
+
+function keyReleased(){
+  if(keyCode===DOWN_ARROW){
+    cannonBall.shoot()
+  }
+}
